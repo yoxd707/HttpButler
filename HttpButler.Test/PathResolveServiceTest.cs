cba94c62cf29e85e0398374e910dcb5f4e59ba4f
Ulises Castaño <ulises27.000@gmail.com>
2025-11-20 21:29:30 -0300

    Agregados casos de test para el PathResolveService

--- HttpButler.Test/PathResolveServiceTest.cs
+++ HttpButler.Test/PathResolveServiceTest.cs
@@ -5,19 +5,37 @@ namespace HttpButler.Test;
 [TestClass]
 public sealed class PathResolveServiceTest
 {
-    [TestMethod]
-    public void TestMethod1()
+    private static readonly object[] ParametersTestData = new object[]
     {
-        var expected = "/users/A01/photos/7d36b9155";
-
-        var pathResolveService = new PathResolveService();
-
-        var route = "/users/{userId}/photos/{photoId}";
-        var parameters = new
-        {
+        new {
             userId = "A01",
-            photoId = "7d36b9155"
-        };
+            photoId = "7d36b9155",
+        },
+        new {
+            productId = "12345",
+        },
+        new {
+            orderId = "98765",
+            itemId = 4321,
+        },
+        new {
+            lang = "es-es",
+            product = "dotnet",
+            category = "csharp",
+            group = "language-reference",
+            post = "language-versioning",
+        },
+    };
+
+    [TestMethod]
+    [DataRow("/users/A01/photos/7d36b9155/", "/users/{userId}/photos/{photoId}", 0)]
+    [DataRow("/products/12345/details/", "/products/{productId}/details", 1)]
+    [DataRow("/orders/98765/items/4321/", "/orders/{orderId}/items/{itemId}/", 2)]
+    [DataRow("learn.microsoft.com/es-es/dotnet/csharp/language-reference/language-versioning/", "learn.microsoft.com/{lang}/{product}/{category}/{group}/{post}/", 3)]
+    public void ResolveUri_RouteParams_ShouldBeCorrect(string expected, string route, int parametersIndex)
+    {
+        var pathResolveService = new PathResolveService();
+        var parameters = ParametersTestData[parametersIndex];
 
         var uri = pathResolveService.ResolveUri(route, parameters);
         var result = uri.ToString();
@@ -26,17 +44,29 @@ public sealed class PathResolveServiceTest
     }
 
     [TestMethod]
-    public void TestMethod2()
+    [DataRow("/users/?userId=A01&photoId=7d36b9155", "/users/", 0)]
+    [DataRow("/products/?productId=12345", "/products", 1)]
+    [DataRow("/orders/?orderId=98765&itemId=4321", "/orders/?{orderId}", 2)]
+    [DataRow("learn.microsoft.com/?lang=es-es&product=dotnet&category=csharp&group=language-reference&post=language-versioning", "learn.microsoft.com/?{lang}&{product}&", 3)]
+    public void ResolveUri_QueryParams_ShouldBeCorrect(string expected, string route, int parametersIndex)
     {
-        var expected = "/users/A01/photos";
-
         var pathResolveService = new PathResolveService();
+        var parameters = ParametersTestData[parametersIndex];
 
-        var route = "/users/{userId}/photos";
-        var parameters = new
-        {
-            userId = "A01"
-        };
+        var uri = pathResolveService.ResolveUri(route, parameters);
+        var result = uri.ToString();
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    [DataRow("/users/A01/?photoId=7d36b9155", "/users/{userId}/", 0)]
+    [DataRow("/orders/98765/?itemId=4321", "/orders/{orderId}", 2)]
+    [DataRow("learn.microsoft.com/es-es/?product=dotnet&category=csharp&group=language-reference&post=language-versioning", "learn.microsoft.com/{lang}/?{product}&", 3)]
+    public void ResolveUri_RouteParamsAndQueryParams_ShouldBeCorrect(string expected, string route, int parametersIndex)
+    {
+        var pathResolveService = new PathResolveService();
+        var parameters = ParametersTestData[parametersIndex];
 
         var uri = pathResolveService.ResolveUri(route, parameters);
         var result = uri.ToString();
